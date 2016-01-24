c7b621d0ebd7a859667272b747d9e5f032ca9b22
simeydotme <simey.me@gmail.com>
2016-01-24 23:13:31 +0800

    [ gulp ] - set up gulp for initial building of default project

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+
+var fs = require("fs"),
+    del = require("del"),
+    gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    nano = require("gulp-cssnano"),
+    size = require("gulp-size"),
+    header = require("gulp-header"),
+    rename = require("gulp-rename"),
+    prefix = require("gulp-autoprefixer"),
+    sourcemaps = require("gulp-sourcemaps"),
+
+    banner = fs.readFileSync("./src/header.txt", "utf-8");
+
+gulp.task( "clean", function() {
+
+    del( "./dist" );
+
+});
+
+gulp.task( "max", ["clean"], function() {
+
+    gulp.src("./src/eezo.scss")
+
+        .pipe( sass({ 
+            outputStyle: "nested"
+        }).on( "error", sass.logError ) )
+        .pipe( prefix() )
+        .pipe( header( banner ) )
+        .pipe( size({ title: "~~ reg: "}) )
+
+        .pipe( gulp.dest("./dist") );
+
+});
+
+gulp.task( "min", ["clean"], function() {
+
+    gulp.src("./src/eezo.scss")
+
+        .pipe( sourcemaps.init() )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( prefix() )
+        .pipe( header( banner ) )
+        .pipe( nano({ mergeLonghand: false }) )
+        .pipe( size({ title: "~~ min: "}) )
+        .pipe( size({ title: "~~ gzip: ", gzip: true }) )
+        .pipe( rename("eezo.min.css") )
+        .pipe( sourcemaps.write("./") )
+
+        .pipe( gulp.dest("./dist") );
+
+});
+
+gulp.task("default", ["min", "max"]);
